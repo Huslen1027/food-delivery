d3d7857ef7cdb32d51829a553730aa9db80e317b
Huslen1027 <hvslenhvslen566@gmail.com>
2024-03-25 19:40:03 +0800

    mai

--- src/pages/forgotpass.tsx
+++ src/pages/forgotpass.tsx
@@ -1,6 +1,13 @@
 import { Grid } from "@mui/material";
 
+// import { useState } from "react";
+import StepThree from "@/components/forgotPass/StepThree";
 export const Page = () => {
-  return <Grid></Grid>;
+  // const [showload, setshowload] = useState("signup");
+  return (
+    <Grid>
+      <StepThree />
+    </Grid>
+  );
 };
 export default Page;
