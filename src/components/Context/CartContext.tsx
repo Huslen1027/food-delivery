317125cc890d4c7a1d1a01dab4f976a00bf55e21
Huslen1027 <hvslenhvslen566@gmail.com>
2024-04-08 20:09:26 +0800

    mai

--- src/components/Context/CartContext.tsx
+++ src/components/Context/CartContext.tsx
@@ -11,17 +11,14 @@ interface FoodData {
   stock: number;
   count: number;
 }
-
 // FoodContext iin type iin zaaj ugnu
 interface CartContextType {
   cartFoods: FoodData[];
   setCartFoods: React.Dispatch<React.SetStateAction<FoodData[]>>;
 }
 const CartContext = createContext<CartContextType>({} as CartContextType);
-
 // Custom hook uusgej bn useContext ashiglaj
 export const useCartItems = () => useContext(CartContext);
-
 export const CartContextProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
